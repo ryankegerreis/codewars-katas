dcc1d484a760b92882157c660e0549c40acfe8a9
Ryan Kegerreis <ryankegerreis@icloud.com>
2020-01-17 11:04:30 -0500

    Unfinished zombies

---
+++ zombies.js
@@ -0,0 +1,29 @@
+// function zombie_shootout(zombies, range, ammo) {
+//   // let zombieAttack = 
+//   setInterval(() => {
+//     range -= .5; zombies -= 1; ammo -= 1
+//     console.log(zombies, range, ammo)
+//     if (range === 0) {
+//       // clearInterval(zombieAttack)
+//       console.log(`You shot ${zombies} before being eaten: overwhelmed.`)
+//       return `You shot ${zombies} before being eaten: overwhelmed.`
+//       break;
+//     } else if (ammo === 0) {
+//       console.log(`You shot ${zombies} before being eaten: ran out of ammo.`)
+//       // clearInterval(zombieAttack)
+//       return `You shot ${zombies} before being eaten: ran out of ammo.`
+//     } else if (zombies === 0) {
+//       // clearInterval(zombieAttack)
+//       return `You shot all ${zombies} zombies.`
+//     }
+//   }, 1000)
+// }
+
+
+function zombie_shootout(zombies, range, ammo) {
+
+  for ()
+
+}
+
+zombie_shootout(10, 3, 3)
